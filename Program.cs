455b484edc0ee32c22ea38e2cd0446fc30a7fded
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-06 20:08:29 -0300

    Eu amo divisões

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+double nume, deno, divisor;
+
+Console.Write("Digite o numerador....: ");
+nume = Convert.ToDouble(Console.ReadLine())!;
+
+Console.Write("Digite o denominador..: ");
+deno = Convert.ToDouble(Console.ReadLine())!;
+
+divisor = nume / deno;
+
+if (deno <= 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Não é possível dividir por zero.");
+            Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{nume} dividido por {deno} é {divisor}");
+            Console.ResetColor();
+}
